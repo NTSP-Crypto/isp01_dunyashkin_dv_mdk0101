16bf9fe100321af278a283db449a406e5abbca17
Keton Sector <KetonSector@protonmail.com>
2020-03-01 18:39:56 +0800

    Add work 12_02_2020

---
+++ self_task/work_12_02_2020/projects/ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vector3D myVector = new Vector3D(35.50, 87.90, 35.10);
+            Vector3D otherVector = new Vector3D(50.50, 32.35, 12.70);
+            Console.WriteLine($"Вывод изначального вектора: {myVector.ToString()}");
+
+            Console.WriteLine($"Длина вектора: {myVector.GetLengt()}");
+
+            Console.WriteLine($"Cкалярное произведение: {myVector.ScalarProduct(otherVector)}");
+
+            Console.WriteLine($"Векторное произведение: {myVector.VectorProduct(otherVector)}");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ self_task/work_12_02_2020/projects/ConsoleApp2/ConsoleApp2/Vector3D.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Vector3D
+    {
+        double X { get; set; }
+        double Y { get; set; }
+        double Z { get; set; }
+
+        public Vector3D(double x, double y, double z)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+        }
+
+        public Vector3D VectorProduct(Vector3D other)
+        {
+            return new Vector3D(Y * other.Z - Z * other.Y,
+                                Z * other.X - X * other.Z,
+                                X * other.Y - Y * other.X);
+        }
+
+        public double GetLengt()
+        {
+            return Math.Sqrt(Math.Pow(X, 2) + Math.Pow(Y, 2) + Math.Pow(Z, 2));
+        }
+
+        public double ScalarProduct(Vector3D Vector)
+        {
+            return (X * Vector.X + Y * Vector.Y + Z * Vector.Z);
+        }
+
+        public override string ToString()
+        {
+            return $"Vector (X: {X}, Y: {Y}, Z: {Z})";
+        }
+    }
+}
