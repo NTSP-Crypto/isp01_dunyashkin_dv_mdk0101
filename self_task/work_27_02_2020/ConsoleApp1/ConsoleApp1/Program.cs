9bc9e7112b211be3d01cd707d2a87e9fcfb48485
Keton Sector <KetonSector@protonmail.com>
2020-03-01 19:17:00 +0800

    Add work_27_02_2020

---
+++ self_task/work_27_02_2020/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = @"D:\Private\Git\isp01_dunyashkin_dv_mdk0101\self_task\work_27_02_2020\ConsoleApp1\ConsoleApp1\sample_text_file.txt";
+            if(!File.Exists(filePath))
+            {
+                Console.WriteLine("Файла нет!");
+                Console.ReadKey();
+                return;
+            }
+            FileStream fstream = new FileStream(@"D:\Private\Git\isp01_dunyashkin_dv_mdk0101\self_task\work_27_02_2020\ConsoleApp1\ConsoleApp1\sample_text_file.txt", FileMode.Open);
+
+            //1 Method
+            Console.WriteLine($"Количество вхождений заданного символа : {Stream.ChekerInt(fstream, '2')}");
+            Console.WriteLine();
+
+            //2 Method
+            Console.WriteLine($"Количество строк в текстовом документе : {Stream.ChekerNumberOfLines(fstream)}");
+            Console.WriteLine();
+
+            //3 Method
+            Console.WriteLine($"Количество цифровых символов : {Stream.ChekerNumbers(fstream)}");
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ self_task/work_27_02_2020/ConsoleApp1/ConsoleApp1/Stream.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.IO;
+
+namespace ConsoleApp1
+{
+    class Stream
+    {
+        public static int ChekerInt(FileStream fstream, int symbol)
+        {
+            StreamReader reader = new StreamReader(fstream);
+            int count = 0;
+            
+            while(!reader.EndOfStream)
+            {
+                char isSymbol = (char)reader.Read();
+                if(isSymbol == symbol)
+                {
+                    count++;
+                }
+            }
+            fstream.Seek(0, SeekOrigin.Begin);
+            return count;
+        }
+
+        public static int ChekerNumberOfLines(FileStream fstream)
+        {
+            StreamReader reader = new StreamReader(fstream);
+            int count = 0;
+
+            while (!reader.EndOfStream)
+            {
+                char isSymbol = (char)reader.Read();
+                if (isSymbol == '\n')
+                {
+                    count++;
+                }
+            }
+
+            fstream.Seek(0, SeekOrigin.Begin);
+            return count;
+        }
+
+        public static int ChekerNumbers(FileStream fstream)
+        {
+            StreamReader reader = new StreamReader(fstream);
+            int count = 0;
+
+            while (!reader.EndOfStream)
+            {
+                if (char.IsDigit((char)reader.Read()))
+                    count++;
+            }
+
+            fstream.Seek(0, SeekOrigin.Begin);
+            return count;
+        }
+    }
+}
